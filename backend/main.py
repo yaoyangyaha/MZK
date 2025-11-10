7b23ecf2022ef41470999a9bc70d7c3bd1ac752d
yaoyangyaha <yaoyangyaha@163.com>
2025-11-10 21:49:29 +0800

    build project

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, request
+import jwt
+from flask_cors import CORS
+from openpyxl import load_workbook
+from waitress import serve
+
+app = Flask(__name__)
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    #do some thing in here
+    print("login")
